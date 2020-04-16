bd7a2ffb83238779e037fd3ca1df153a0b90a4ce
MohanSairam423 <48183441+MohanSairam423@users.noreply.github.com>
2020-04-16 18:12:37 +0530

    Test Commit

--- src/test/java/com/slokam/automation/opencart/testscripts/SampleTest.java
+++ src/test/java/com/slokam/automation/opencart/testscripts/SampleTest.java
@@ -11,6 +11,22 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class SampleTest {
+	
+	@Test
+	public void testFireFox() {
+		WebDriver driver = newFirefoxDriver();
+		//it will open the goggle page
+		driver.get("http://google.in"); 
+		//we expect the title “Google “ should be present 
+		String Expectedtitle = "Google";
+		//it will fetch the actual title 
+		String Actualtitle = driver.getTitle();
+		System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+		//it will compare actual title and expected title
+		Assert.assertEquals(Actualtitle, Expectedtitle);
+		//print out the result
+		System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+	}
 
 	//@Test
 	public void testChrome() {
