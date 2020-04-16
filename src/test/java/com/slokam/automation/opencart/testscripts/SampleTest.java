1e291883c1cc066f8ea82abb48e0fa9c893835cd
MohanSairam423 <48183441+MohanSairam423@users.noreply.github.com>
2020-04-16 18:01:07 +0530

    Test Commit

--- src/test/java/com/slokam/automation/opencart/testscripts/SampleTest.java
+++ src/test/java/com/slokam/automation/opencart/testscripts/SampleTest.java
@@ -20,18 +20,19 @@ public class SampleTest {
 		Capabilities cap = DesiredCapabilities.chrome();
 		URL url = null;
 		try {
-			url = new URL("http://localhost:4444/wd/hub");
+			//url = new URL("http://localhost:4444/wd/hub");
+			url = new URL("http://www.google.com");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		WebDriver driver = new RemoteWebDriver(url,cap);
 		
-		driver.get("http://localhost/opencart/");
+		driver.get("http://www.google.com");
 		driver.quit();
 		System.out.println("End of my testcase - testChrome " + System.currentTimeMillis());
 	}
 
-	@Test
+	//@Test
 	public void testFirefox() {
 		System.out.println("Starting the testcase - testFirefox " + System.currentTimeMillis());
 		System.out.println("I am testFirefox");
@@ -48,7 +49,7 @@ public class SampleTest {
 		System.out.println("End of my testcase - testFirefox " + System.currentTimeMillis());
 	}
 
-	@Test
+	//@Test
 	public void testChrome1() {
 		System.out.println("Starting the testcase - testChrome " + System.currentTimeMillis());
 		System.out.println("I am testChrome1");
@@ -65,7 +66,7 @@ public class SampleTest {
 		System.out.println("End of my testcase - testChrome1 " + System.currentTimeMillis());
 	}
 
-	@Test
+	//@Test
 	public void testFirefox1() {
 		System.out.println("Starting the testcase - testFirefox " + System.currentTimeMillis());
 		System.out.println("I am testFirefox1");
@@ -82,7 +83,7 @@ public class SampleTest {
 		System.out.println("End of my testcase - testFirefox1 " + System.currentTimeMillis());
 	}
 
-	@Test
+	//@Test
 	public void testChrome2() {
 		System.out.println("Starting the testcase - testChrome " + System.currentTimeMillis());
 		System.out.println("I am testChrome2");
@@ -99,7 +100,7 @@ public class SampleTest {
 		System.out.println("End of my testcase - testChrome2 " + System.currentTimeMillis());
 	}
 
-	@Test
+	//@Test
 	public void testFirefox2() {
 		System.out.println("Starting the testcase - testFirefox " + System.currentTimeMillis());
 		System.out.println("I am testFirefox2");
