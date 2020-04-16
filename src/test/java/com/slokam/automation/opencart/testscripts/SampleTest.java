ee60cf32318d9b42d989c2fcdfbf0cd711658d49
MohanSairam423 <48183441+MohanSairam423@users.noreply.github.com>
2020-04-16 18:18:25 +0530

    Test Commit

--- src/test/java/com/slokam/automation/opencart/testscripts/SampleTest.java
+++ src/test/java/com/slokam/automation/opencart/testscripts/SampleTest.java
@@ -14,7 +14,7 @@ import org.testng.Assert;
 public class SampleTest {
 	
 	@Test
-	public void testChrome1() {
+	public void testChrome2() {
 		WebDriver driver = new ChromeDriver();
 		//it will open the goggle page
 		driver.get("http://google.in"); 
