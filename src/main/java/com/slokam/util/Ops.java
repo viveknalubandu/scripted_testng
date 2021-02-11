352959222b621fefecd477e873bf25818d404c22
viveknalubandu <vivek091@gmail.com>
2021-02-11 09:56:36 -0800

    Master C1

--- src/main/java/com/slokam/util/Ops.java
+++ src/main/java/com/slokam/util/Ops.java
@@ -3,7 +3,7 @@ package com.slokam.util;
 import java.util.ArrayList;
 
 public class Ops {
-
+  // Test
 	
 	public static void main(String[] args) {
 		ArrayList<Ticket> ticketList = new ArrayList<Ticket>();
